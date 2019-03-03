dd492b78d3ca85d84e8414c81638625dcdbf691a
ken <1601790617@qq.com>
2019-03-03 13:54:13 +0800

    防止非法访问jsp页面

---
+++ src/main/java/com/hzh/answer/web/filter/LoginFilter.java
@@ -0,0 +1,65 @@
+package com.hzh.answer.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.hzh.answer.domain.SysUser;
+
+/**
+ * 过滤非法访问，没有登录的人不能访问除登录页外的其他页面
+ * 且所有页面都通过action的方式访问
+ * @author ken
+ *
+ */
+public class LoginFilter implements Filter {
+
+	private HttpServletRequest request;
+	private HttpServletResponse response;
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("jsp过滤器结束");
+	}
+
+	@Override
+	public void doFilter(ServletRequest request1, ServletResponse response1, FilterChain chain)
+			throws IOException, ServletException {
+		this.request = (HttpServletRequest) request1;
+		this.response = (HttpServletResponse) response1;
+//		SysUser sysUser = (SysUser) request.getSession().getAttribute("existUser");
+//		System.out.println(sysUser.getUsername());
+		boolean isJsp = request.getRequestURI().contains("jsp");
+		if(isJsp) {
+			System.out.println("是jsp");
+			boolean isLoginJsp = request.getRequestURI().contains("/answerSystem/public/login.jsp");
+			// 过滤掉所有不是login.jsp的jspURL
+			if (!isLoginJsp) {
+				System.out.println("是login.jsp");
+				response.sendRedirect("/answerSystem/public/login.jsp");
+				return;
+			}
+		} else if(!request.getRequestURI().contains(".action")) {
+			response.sendRedirect("/answerSystem/public/login.jsp");
+			return;
+		} 
+		
+		chain.doFilter(request, response);
+
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("jsp过滤器开始");
+	}
+
+}
